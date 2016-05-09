a1c61f03e402308ff19eca13aa2805270028cce3
welldefinedvariables <lingeorge0@gmail.com>
2016-05-09 13:50:59 -0400

    Final

---
+++ final/project/json.js
@@ -0,0 +1,99 @@
+{
+    "feed": [{
+        "link": "http://diveintohtml5.info/",
+        "title": "Dive into HTML5",
+        "hook": "Dive Into HTML5 elaborates on a hand-picked selection of features from the HTML5 specification and other fine standards.",
+        "source": "Mark Pilgrim with contributions from the community",
+        "Tags": "Web, HTML5",
+        "Author": "Mark Pilgrim",
+        "Type": "Tutorial",
+        "Language": "English",
+        "Published": "2009",
+        "Description": "HTML5 is creating more and more a name for itself in our industry, but while it excites those on the cutting edge of web technology, many are left feeling uncertain about it. Its ongoing development has been victim of politics, fragmentation and more, leaving few to have a good grasp of its current status. However, a lot of the technologies that make up HTML5 (and more) have become mature, even implemented across all the latest browsers--but did you know that? If you've kept an arms length to everything going on with HTML5, now is the time to dive into its waters and explore.",
+        "Related": "None",
+        "Alternative Names": "HTML5: Up and Running",
+        "GoodReads Rating": "4.20",
+        "Amazon Rating": "3.80"
+    }, {
+        "link": "http://blog.cryptographyengineering.com/2011/09/what-is-random-oracle-model-and-why.html",
+        "title": "What is the Random Oracle Model and why?",
+        "hook": "This is the first in a series of posts on the Random Oracle Model.",
+        "source": "Matthew Green",
+        "Tags": "Cryptography",
+        "Author": "Matthew Green",
+        "Type": "Lesson",
+        "Language": "English",
+        "Description": "This is the first in a series of posts on the Random Oracle Model.  This might get a little wonky for some people's tastes, so if you're not interested in provable security, no problem.  I'll be back with more on software and physical security once I get this out of my system."
+    }, {
+        "link": "https://www.lynda.com/Foundation-tutorials/Up-Running-Foundation-5/161085-2.html",
+        "title": "Up and Running with Foundation 5",
+        "hook": "In this advanced course, learn to build responsive websites with Foundation—the clean, simple design framework that uses integrated CSS and JavaScript.",
+        "source": "Jen Kramer",
+        "Tags": "Web CSS3",
+        "Author": "Jen Kramer",
+        "Type": "Tutorial",
+        "Language": "English",
+        "Description": "In this advanced course, learn to build responsive websites with Foundation—the clean, simple design framework that uses integrated CSS and JavaScript. Jen Kramer covers Foundation's twelve-column grid system, including the small, medium, and large grid options that make layouts easy to assemble. Then learn how to adjust Foundation's default CSS styling to make your buttons, thumbnails, and other page elements look just the way you want, and add top, side, and off-canvas navigation bars. Jen also shows you how to work with cool JavaScript-based effects like Orbit (an image carousel), Reveal (a modal window plugin), and Interchange (offering responsive image management)."
+    }, {
+        "link": "http://www.w3schools.com/",
+        "title": "W3Schools",
+        "hook": "W3Schools is a web developers site, with tutorials and references on web development languages such as HTML, CSS, JavaScript, PHP, SQL, and JQuery, covering most aspects of web programming.",
+        "source": "W3Schools",
+        "Tags": "Web HTML5 CSS3 JavaScript",
+        "Type": "Reference",
+        "Language": "English",
+        "Description": "Good enough."
+    }, {
+        "link": "http://jquery.com/",
+        "title": "What is jQuery?",
+        "hook": "jQuery is a fast, small, and feature-rich JavaScript library.",
+        "source": "jQuery",
+        "Tags": "Web JavaScript",
+        "Type": "Library",
+        "Language": "English",
+        "Description": "jQuery is a fast, small, and feature-rich JavaScript library. It makes things like HTML document traversal and manipulation, event handling, animation, and Ajax much simpler with an easy-to-use API that works across a multitude of browsers. With a combination of versatility and extensibility, jQuery has changed the way that millions of people write JavaScript."
+    }, {
+        "link": "http://bradfrost.com/blog/post/atomic-web-design/",
+        "title": "Atomic Design",
+        "hook": "We’re not designing pages, we’re designing systems of components.—Stephen Hay",
+        "source": "Brad Frost",
+        "Tags": "Web",
+        "Author": "Brad Frost",
+        "Type": "Tutorial",
+        "Language": "English",
+        "Description": "As the craft of Web design continues to evolve, we’re recognizing the need to develop thoughtful design systems, rather than creating simple collections of web pages."
+    }, {
+        "link": "https://www.smashingmagazine.com/2013/03/customizing-bootstrap/",
+        "title": "Customizing Bootstrap",
+        "hook": "Twitter’s Bootstrap has taken off like a rocket since its release a year ago. The popular CSS framework supplies a responsive grid system, pre-styled components and JavaScript plugins to a parade of websites.",
+        "source": "Thomas Park",
+        "Tags": "Web CSS3",
+        "Author": "Thomas Park",
+        "Type": "Tutorial",
+        "Language": "English",
+        "Description": "You don’t have to stick with the defaults. This article shows some ways to customize Bootstrap to fit your needs, whether it’s a tweak to a button or a full-fledged theme."
+    }, {
+        "link": "https://developers.google.com/maps/documentation/javascript/",
+        "title": "Google Maps JavaScript API",
+        "hook": "Customize maps with your own content and imagery.",
+        "source": "Google",
+        "Tags": "Web JavaScript",
+        "Type": "API",
+        "Language": "English",
+        "Description": "Start with the maps your users love. Add the data developers trust. Build a custom map for your site using styled maps, 3D buildings, indoor floor plans, multi-modal directions and more."
+    }]
+}
+
+// {
+// 	"link":"",
+// 	"title":"",
+// 	"hook":"",
+// 	"source":"",
+// 	"Tags": "",
+// 	"Author": "",
+// 	"Type": "",
+// 	"Language": "",
+// 	"Description":""
+
+
+// }
---
+++ final/project/test.js
@@ -0,0 +1,5 @@
+document.getElementById('container').onscroll = function(e) {
+    console.log("scrolling");
+    console.log($(this).scrollTop());
+};
+
